04ac8352e76e8a340f88d060ebd02d3b649da6ae
tinygone Lee <tinygone@qq.com>
2017-09-18 23:09:27 +0800

    default project of create react app

---
+++ src/SingerCategory.js
@@ -0,0 +1,16 @@
+/**
+ * Created by liqigang on 2017/9/18.
+ */
+
+import React, {Component} from 'react';
+
+class SingerCategory extends Component {
+    render() {
+        return (
+            <div>SingerCategory</div>
+        );
+
+    }
+}
+
+export default SingerCategory;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import SingerCategory from './SingerCategory';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<SingerCategory />, document.getElementById('root'));
+registerServiceWorker();
+//ReactDOM.render(<h1>Hello World</h1>, document.getElementById('root'));
