51ebb23d8c8f1f10cd133fa97df50b1b358dd808
baby <baby@vm_152_71_centos.localdomain>
2017-09-19 22:39:13 +0800

    http://blog.csdn.net/a153375250/article/details/52667739

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import SingerCategory from './SingerCategory';;
-//import ManageSystem from "./ManageSystem"
+import ManageSystem from "./ManageSystem"
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<SingerCategory />, document.getElementById('root'));
+ReactDOM.render(<ManageSystem />, document.getElementById('root'));
 registerServiceWorker();
 //ReactDOM.render(<h1>Hello World</h1>, document.getElementById('root'));
